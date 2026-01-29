b1b5ccad6845da344d2963b8820b07ab94596575
Hugo Alliaume <hugo@alliau.me>
2026-01-29 23:09:37 +0100

    [Toolkit][Shadcn] Add documentation for Toolkit recipe

--- assets/toolkit-shadcn.js
+++ assets/toolkit-shadcn.js
@@ -3,8 +3,10 @@ import { startStimulusApp } from '@symfony/stimulus-bundle';
 import Accordion from '@symfony/ux-toolkit/kits/shadcn/accordion/assets/controllers/accordion_controller.js';
 import AlertDialog from '@symfony/ux-toolkit/kits/shadcn/alert-dialog/assets/controllers/alert_dialog_controller.js';
 import Dialog from '@symfony/ux-toolkit/kits/shadcn/dialog/assets/controllers/dialog_controller.js';
+import Tooltip from '@symfony/ux-toolkit/kits/shadcn/tooltip/assets/controllers/tooltip_controller.js';
 
 const app = startStimulusApp();
 app.register('accordion', Accordion);
 app.register('alert-dialog', AlertDialog);
 app.register('dialog', Dialog);
+app.register('tooltip', Tooltip);

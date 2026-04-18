a10d2b6ad3e68f61d975acaa713af802471f61f9
Hugo Alliaume <hugo@alliau.me>
2026-04-18 15:00:35 +0200

    Add Collapsible Stimulus controller

--- assets/toolkit-shadcn.js
+++ assets/toolkit-shadcn.js
@@ -2,6 +2,7 @@ import './styles/toolkit-shadcn.css';
 import { startStimulusApp } from '@symfony/stimulus-bundle';
 import Accordion from '@symfony/ux-toolkit/kits/shadcn/accordion/assets/controllers/accordion_controller.js';
 import AlertDialog from '@symfony/ux-toolkit/kits/shadcn/alert-dialog/assets/controllers/alert_dialog_controller.js';
+import Collapsible from '@symfony/ux-toolkit/kits/shadcn/collapsible/assets/controllers/collapsible_controller.js';
 import Dialog from '@symfony/ux-toolkit/kits/shadcn/dialog/assets/controllers/dialog_controller.js';
 import Tabs from '@symfony/ux-toolkit/kits/shadcn/tabs/assets/controllers/tabs_controller.js';
 import Tooltip from '@symfony/ux-toolkit/kits/shadcn/tooltip/assets/controllers/tooltip_controller.js';
@@ -10,6 +11,7 @@ import Toggle from '@symfony/ux-toolkit/kits/shadcn/toggle/assets/controllers/to
 const app = startStimulusApp();
 app.register('accordion', Accordion);
 app.register('alert-dialog', AlertDialog);
+app.register('collapsible', Collapsible);
 app.register('dialog', Dialog);
 app.register('tabs', Tabs);
 app.register('tooltip', Tooltip);

dc9419be97abaefbabdde0bc3eb18fa5e0bc373e
Pascal CESCON - Amoifr <pascal.cescon@gmail.com>
2026-04-27 09:55:18 +0200

    [Toolkit][Shadcn] Register menubar_controller

--- importmap.php
+++ importmap.php
@@ -195,6 +195,9 @@ return [
     '@symfony/ux-toolkit/kits/shadcn/dialog/assets/controllers/dialog_controller.js' => [
         'path' => './vendor/symfony/ux-toolkit/kits/shadcn/dialog/assets/controllers/dialog_controller.js',
     ],
+    '@symfony/ux-toolkit/kits/shadcn/menubar/assets/controllers/menubar_controller.js' => [
+        'path' => './vendor/symfony/ux-toolkit/kits/shadcn/menubar/assets/controllers/menubar_controller.js',
+    ],
     '@symfony/ux-toolkit/kits/shadcn/tooltip/assets/controllers/tooltip_controller.js' => [
         'path' => './vendor/symfony/ux-toolkit/kits/shadcn/tooltip/assets/controllers/tooltip_controller.js',
     ],
